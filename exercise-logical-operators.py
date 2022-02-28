35a7c2ed40f17a5de0630d259f068c92e189db7b
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-28 13:01:19 -0600

    Add files via upload

---
+++ exercise-logical-operators.py
@@ -0,0 +1,36 @@
+is_magician = True
+is_expert = False 
+
+# chek if magician AND expert: "you are a master magician"
+
+if is_expert and is_magician:
+    print("you are a master magician")
+
+# check if magician but not expert: 
+# "at least you're getting there"
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+
+# if you're not a magician: "You need magic powers"
+
+elif not is_magician: 
+    print("You need magic powers")
+
+is_magician = False
+is_expert = True
+
+# chek if magician AND expert: "you are a master magician"
+
+if is_expert and is_magician:
+    print("you are a master magician")
+
+# check if magician but not expert: 
+# "at least you're getting there"
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+
+# if you're not a magician: "You need magic powers"
+
+elif not is_magician: 
+    print("You need magic powers")
+
